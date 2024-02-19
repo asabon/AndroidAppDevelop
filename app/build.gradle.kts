1d417392793a95e1d44a39fc14822ff3f629125b
Takafumi Asahara <asabon.t@gmail.com>
2024-02-19 19:14:12 +0900

    Disable appcompat

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,7 +79,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.0")
 
     // Jetpack Compose (Additional)
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    //implementation("androidx.appcompat:appcompat:1.6.1")
     //implementation("androidx.compose.foundation:foundation:1.6.1")
     //implementation("androidx.compose.foundation:foundation-layout:1.6.1")
     //implementation("androidx.compose.foundation:foundation-layout-android:1.6.1")
