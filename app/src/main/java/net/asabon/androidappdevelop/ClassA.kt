7d5f7903dc2f28f0039f3ae0c0896882dcab2986
Takafumi Asahara <asabon.t@gmail.com>
2024-03-04 06:34:50 +0900

    Add ClassA

---
+++ app/src/main/java/net/asabon/androidappdevelop/ClassA.kt
@@ -0,0 +1,12 @@
+package net.asabon.androidappdevelop
+
+class ClassA {
+    fun calculate(a: Int, b: Int): Int {
+        val array = arrayOf(
+            intArrayOf(  1,   2,   3,   4,   5), // コメントA
+            intArrayOf(  6,   7,   8,   9,  10), // コメントB
+            intArrayOf(100, 200, 300, 400, 500)  // コメントC
+        )
+        return array[a][b]
+    }
+}
