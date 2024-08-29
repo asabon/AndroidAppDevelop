8062157516f8a9d5e85bbac70c9b6bb66c61fb4b
Takafumi Asahara <asabon.t@gmail.com>
2024-08-30 08:10:39 +0900

    fix warnings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     defaultConfig {
         applicationId = "net.asabon.androidappdevelop"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
 
