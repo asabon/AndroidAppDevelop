913a3a27fb14187116a18888521961262a977025
Takafumi Asahara <asabon.t@gmail.com>
2024-12-26 15:46:27 +0900

    update lib and sdk version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "net.asabon.androidappdevelop"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "net.asabon.androidappdevelop"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
